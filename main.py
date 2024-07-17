4c494e369ab6dbe9f64b8fc3400894c377148309
Moatasimzz <moatasimalajami@gmail.com>
2024-07-17 19:01:34 +0300

    Add city's menu function

--- main.py
+++ main.py
@@ -35,3 +35,26 @@ def driver_menu(drivers, cities):
             print("Invalid input !!, please try again")
 
 
+def cities_menu(drivers, cities):
+    while True:
+        print("CITIES MENU")
+        print("Enter:")
+        print("1- To show all cities")
+        print("2- To print neighboring cities")
+        print("3- To print drivers delivering to a city")
+        print("4- To go back to main menu")
+        choice = input("Your choice: ")
+
+        if choice == "1":
+            show_cities(cities)
+        elif choice == "2":
+            print_neighboring_cities(cities)
+        elif choice == "3":
+            print_drivers_delivering_to_city(drivers, cities)
+        elif choice == "4":
+            print("Going back to main menu...")
+            break
+        else:
+            print("Invalid input !!, please try again :")
+
+            
